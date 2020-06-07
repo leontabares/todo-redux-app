0f4c8678412091abcd2a12bd2c24d9338544c544
Leon <leon.tabares.z@gmail.com>
2020-06-07 14:34:13 -0500

    primer commit

---
+++ src/app/todos/todo-footer/todo-footer.component.ts
@@ -0,0 +1,39 @@
+import { Component, OnInit } from '@angular/core';
+import { Store } from '@ngrx/store';
+import { AppState } from 'src/app/app.reducer';
+import * as actionsFilters from 'src/app/filtro/filtro.actions';
+import * as actionsTodo from '../todo.actions';
+
+@Component({
+  selector: 'app-todo-footer',
+  templateUrl: './todo-footer.component.html',
+  styleUrls: ['./todo-footer.component.css']
+})
+export class TodoFooterComponent implements OnInit {
+
+  filtroActual: actionsFilters.filtrosValidos= 'todos';
+  filtros: actionsFilters.filtrosValidos[] = ['todos','completados','pendientes'];
+  pendientes: number = 0;
+
+  constructor(private store: Store<AppState>) { }
+
+  ngOnInit(): void {
+    // this.store.select('filtro').subscribe( filtro => {
+    //   this.filtroActual = filtro;
+    // });
+
+    this.store.subscribe( state => {
+      this.filtroActual = state.filtro;
+      this.pendientes = state.todos.filter( todo => !todo.completado).length;
+    });
+  }
+
+  cambiarFiltro(filtro: actionsFilters.filtrosValidos) {
+    this.store.dispatch(actionsFilters.setFiltro({ filtro: filtro }));
+  }
+
+  LimpiarCompletados() {
+    this.store.dispatch(actionsTodo.limpiar());
+  }
+
+}
